a7a18dc097f51313d72c07f589111941a3f5d8a4
beefoo <brian@youaremyjoy.org>
2022-05-17 22:08:54 -0400

    implement first pass at texture and thumb generation

--- ingest.js
+++ ingest.js
@@ -1,5 +1,6 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const sharp = require('sharp');
 const config = require('./config.json');
 
 function readMetadata(cfg, onFinished) {
@@ -15,11 +16,20 @@ function readMetadata(cfg, onFinished) {
     .on('data', (data) => metadata.push(data))
     .on('end', () => {
       onFinished(cfg, metadata);
+      console.log(`Read ${metadata.length} rows from ${cfg.dataFile}`);
     });
 }
 
 function resizeImages(cfg, metadata) {
-
+  metadata.forEach(async (item, i) => {
+    const textureImage = await sharp(`${cfg.imageDirectory}${item.Filename}`)
+      .resize(cfg.textureSize[0], cfg.textureSize[1]);
+    const savedThumbImage = await textureImage
+      .resize(cfg.thumbSize[0], cfg.thumbSize[1])
+      .toFile(`${cfg.targetImageDirectory}/thumb/${i}.jpg`);
+    const savedTextureImage = await textureImage.toFile(`${cfg.targetImageDirectory}/texture/${i}.jpg`);
+    console.log(`Finished ${item.Filename}`);
+  });
 }
 
 function writeMetadata(cfg, metadata) {
