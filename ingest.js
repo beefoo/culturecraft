1a8fd2bf0f2630f5814c1dbb738a5e3e9e3ded3c
Brian Foo <brian@youaremyjoy.org>
2022-05-19 16:10:03 -0400

    ingest refactor: sort rows by index, check for existing images, ensure input image exists

--- ingest.js
+++ ingest.js
@@ -6,13 +6,7 @@ const config = require('./config.json');
 function readMetadata(cfg, onFinished) {
   const metadata = [];
   fs.createReadStream(cfg.dataFile)
-    .pipe(csv({
-      mapValues: ({ header, index, value }) => {
-        const newValue = value;
-        newValue.Index = parseInt(value.Index, 10);
-        return newValue;
-      },
-    }))
+    .pipe(csv())
     .on('data', (data) => metadata.push(data))
     .on('end', () => {
       onFinished(cfg, metadata);
@@ -22,19 +16,36 @@ function readMetadata(cfg, onFinished) {
 
 function resizeImage(cfg, metadata, i) {
   const item = metadata[i];
-  const textureImage = sharp(`${cfg.imageDirectory}${item.Filename}`)
+  const inputImageFilename = `${cfg.imageDirectory}${item.Filename}`;
+  const textureImageFilename = `${cfg.targetImageDirectory}/texture/${i}.jpg`;
+  const thumbImageFilename = `${cfg.targetImageDirectory}/thumb/${i}.jpg`;
+  const continueNext = (i < metadata.length - 1);
+  let processImage = true;
+
+  if (fs.existsSync(textureImageFilename) && fs.existsSync(thumbImageFilename)) {
+    processImage = false;
+    console.log(`${item.Filename} already processed`);
+  } else if (!fs.existsSync(inputImageFilename)) {
+    processImage = false;
+    console.log(`${inputImageFilename} does not exist`);
+  }
+
+  if (!processImage) {
+    if (continueNext) resizeImage(cfg, metadata, i + 1);
+    return;
+  }
+
+  const textureImage = sharp(inputImageFilename)
     .resize(cfg.textureSize[0], cfg.textureSize[1]);
   textureImage
-    .toFile(`${cfg.targetImageDirectory}/texture/${i}.jpg`)
+    .toFile(textureImageFilename)
     .then(() => {
       textureImage
         .resize(cfg.thumbSize[0], cfg.thumbSize[1])
-        .toFile(`${cfg.targetImageDirectory}/thumb/${i}.jpg`)
+        .toFile(thumbImageFilename)
         .then(() => {
           console.log(`Processed ${item.Filename}`);
-          if (i < metadata.length - 1) {
-            resizeImage(cfg, metadata, i + 1);
-          }
+          if (continueNext) resizeImage(cfg, metadata, i + 1);
         });
     });
 }
@@ -57,7 +68,17 @@ function writeMetadata(cfg, metadata) {
   });
 }
 
-readMetadata(config, (cfg, metadata) => {
+readMetadata(config, (cfg, rawData) => {
+  const metadata = rawData.map((item) => {
+    const newItem = { ...item };
+    newItem.Index = parseInt(item.Index, 10);
+    return newItem;
+  });
+  metadata.sort((a, b) => {
+    if (a.Index > b.Index) return 1;
+    if (b.Index > a.Index) return -1;
+    return 0;
+  });
   resizeImages(cfg, metadata);
   writeMetadata(cfg, metadata);
 });
