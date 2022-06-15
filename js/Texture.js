7487fd2b7173c37d8775e2d82a8c89b30d3c76fc
beefoo <brian@youaremyjoy.org>
2022-06-14 22:20:28 -0400

    refactor texture loading

--- js/Texture.js
+++ js/Texture.js
@@ -1,46 +1,68 @@
 class Texture {
   constructor(options = {}) {
-    const defaults = {};
+    const defaults = {
+      url: false,
+    };
     this.options = _.extend({}, defaults, options);
     this.init();
   }
 
   init() {
+    this.isLoading = false;
     this.isLoaded = false;
+    this.isDestroyed = false;
     this.loadedTexture = false;
-    this.url = false;
+    this.url = this.options.url;
   }
 
-  load(url) {
-    if (url !== this.url) {
-      this.isLoaded = false;
-    }
-    const promise = new Promise((resolve, reject) => {
-      if (this.isLoaded) {
-        resolve(this.loadedTexture);
-      } else {
-        this.loadedTexture = PIXI.Texture.from(url);
-        if (this.loadedTexture.valid) {
-          this.isLoaded = true;
-          resolve(this.loadedTexture);
-        } else {
-          this.loadedTexture.on('error', (texture, error) => {
-            reject(new Error(`Error loading texture ${url}: ${error.message}`));
-          });
-          this.loadedTexture.on('loaded', (texture) => {
-            this.isLoaded = true;
-            resolve(this.loadedTexture);
-          });
-        }
-      }
-    });
-    this.url = url;
-    return promise;
-  }
+  destroy() {
+    this.isDestroyed = true;
+    this.isLoaded = false;
 
-  unload() {
     if (this.loadedTexture === false) return;
 
     this.loadedTexture.destroy(true);
+    console.log(`Destroyed ${this.url}`);
+  }
+
+  load() {
+    const { url } = this;
+    if (this.isLoaded) {
+      console.log(`${url} already loaded`);
+      return;
+    }
+    if (this.isDestroyed) {
+      console.log(`${url} already destroyed`);
+      return;
+    }
+    this.isLoading = true;
+    const promise = new Promise((resolve, reject) => {
+      const loadedTexture = PIXI.Texture.from(url);
+      if (loadedTexture.valid) {
+        resolve(loadedTexture);
+      } else {
+        loadedTexture.on('error', (texture, error) => {
+          reject(new Error(`Error loading texture ${url}: ${error.message}`));
+        });
+        loadedTexture.on('loaded', (texture) => {
+          resolve(loadedTexture);
+        });
+      }
+    });
+    promise.then((texture) => {
+      this.loadedTexture = texture;
+      this.isLoaded = true;
+      this.isLoading = false;
+      console.log(`Loaded texture ${this.url}`);
+      // account for race condition: destroyed while loading
+      if (this.isDestroyed) {
+        this.destroy();
+      }
+    }, (error) => {
+      this.isLoaded = false;
+      this.loadedTexture = false;
+      this.isLoading = false;
+      console.log(error.message);
+    });
   }
 }
--- js/TextureManager.js
+++ js/TextureManager.js
@@ -1,12 +1,16 @@
 class TextureManager {
   constructor(options = {}) {
-    const defaults = {};
+    const defaults = {
+      urls: [],
+    };
     this.options = _.extend({}, defaults, options);
     this.init();
   }
 
   init() {
     this.textures = {};
+    this.urls = this.options.urls;
+    this.urlCount = this.urls.length;
   }
 
   loadTexture(url) {
@@ -15,19 +19,35 @@ class TextureManager {
     if (_.has(this.textures, url)) {
       texture = this.textures[url];
     } else {
-      texture = new Texture();
+      texture = new Texture({
+        url,
+      });
       this.textures[url] = texture;
-      texture.load(url);
+      texture.load();
     }
 
     return texture;
+  }
+
+  loadTextureIndex(index) {
+    if (index >= this.urlCount) return;
+
+    const url = this.urls[index];
+    this.loadTexture(url);
   }
 
   unloadTexture(url) {
     if (!_.has(this.textures, url)) return;
 
     const texture = this.textures[url];
-    texture.unload();
+    texture.destroy();
     this.textures = _.omit(this.textures, url);
+  }
+
+  unloadTextureIndex(index) {
+    if (index >= this.urlCount) return;
+
+    const url = this.urls[index];
+    this.unloadTexture(url);
   }
 }
