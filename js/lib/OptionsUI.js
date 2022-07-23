5e460a5b7bfa7399efd4b839569d767239b69750
beefoo <brian@youaremyjoy.org>
2022-07-23 07:50:33 -0400

    implement toggle fullscreen

--- js/lib/OptionsUI.js
+++ js/lib/OptionsUI.js
@@ -9,15 +9,55 @@ class OptionsUI {
 
   init() {
     this.$menuEl = $(this.options.menuEl);
+    this.$navEl = this.$menuEl.closest('.options-nav');
     this.loadListeners();
   }
 
-  loadListeners() {
-    this.$menuEl.find('.toggle-menu').on('click', (e) => OptionsUI.onClickToggleMenu(e));
+  closeMenu() {
+    this.$navEl.removeClass('active');
   }
 
-  static onClickToggleMenu(event) {
-    const $button = $(event.currentTarget);
-    $button.closest('.options-nav').toggleClass('active');
+  static isFullScreen() {
+    const d = document;
+    return (d.fullscreenElement || d.mozFullScreenElement
+      || d.webkitFullscreenElement || d.msFullscreenElement);
+  }
+
+  loadListeners() {
+    const { $menuEl } = this;
+    const $doc = $(document);
+    $menuEl.find('.toggle-menu').on('click', (e) => this.toggleMenu());
+    $menuEl.find('.toggle-fullscreen').on('click', (e) => this.toggleFullscreen());
+    $doc.on('fullscreenchange', (e) => this.onFullscreenChange());
+  }
+
+  onFullscreenChange() {
+    const $button = this.$menuEl.find('.toggle-fullscreen');
+    if (OptionsUI.isFullScreen()) $button.addClass('active');
+    else $button.removeClass('active');
+  }
+
+  toggleFullscreen() {
+    const d = document;
+    const isFullscreen = OptionsUI.isFullScreen();
+
+    if (!isFullscreen) {
+      const el = document.documentElement;
+      if (el.requestFullscreen) el.requestFullscreen();
+      else if (el.msRequestFullscreen) el.msRequestFullscreen();
+      else if (el.mozRequestFullScreen) el.mozRequestFullScreen();
+      else if (el.webkitRequestFullscreen) el.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (isFullscreen) {
+      if (d.exitFullscreen) d.exitFullscreen();
+      else if (d.msExitFullscreen) d.msExitFullscreen();
+      else if (d.mozCancelFullScreen) d.mozCancelFullScreen();
+      else if (d.webkitExitFullscreen) d.webkitExitFullscreen();
+    }
+
+    this.closeMenu();
+  }
+
+  toggleMenu() {
+    this.$navEl.toggleClass('active');
   }
 }
